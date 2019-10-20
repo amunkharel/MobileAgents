f0093bdf6cd88f5ef8a4c03be6371c966b11e47d
Amun Kharel <akharel@cs.unm.edu>
2019-10-20 17:16:17 -0600

    Intial COmmit

---
+++ src/mobileAgents/Main.java
@@ -0,0 +1,17 @@
+package mobileAgents;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
