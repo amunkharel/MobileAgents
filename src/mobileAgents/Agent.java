24e015f76b56d74003a03b9996879c3b2845280d
Amun Kharel <akharel@cs.unm.edu>
2019-10-24 10:07:44 -0600

    Add Design

--- src/mobileAgents/Agent.java
+++ src/mobileAgents/Agent.java
@@ -1,4 +1,17 @@
 package mobileAgents;
 
+import java.util.ArrayList;
+
 public class Agent {
+
+    private Sensor sensor;
+
+    private ArrayList<Sensor> sensors;
+
+    public Agent (Sensor sensor, ArrayList<Sensor> sensors) {
+        this.sensor = sensor;
+        this.sensors = sensors;
+    }
+
+
 }
--- src/mobileAgents/Sensor.java
+++ src/mobileAgents/Sensor.java
@@ -1,4 +1,29 @@
 package mobileAgents;
 
+import java.util.ArrayList;
+
+
 public class Sensor {
+
+    private int xcor;
+
+    private int ycor;
+
+    private int id;
+
+    private ArrayList<Sensor> neighbors;
+
+    public Sensor(int xcor, int ycor, int id) {
+        this.id = id;
+
+        this.xcor = xcor;
+
+        this.ycor = ycor;
+
+        neighbors = new ArrayList<Sensor>();
+    }
+
+    public void setNeighbors(Sensor sensor) {
+        neighbors.add(sensor);
+    }
 }
